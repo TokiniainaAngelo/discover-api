442c561c9b839a34f13d85748bf9093949e003f0
Sandratra <3s.sandratra@gmail.com>
2023-07-27 23:04:05 +0300

    Update user controller

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,18 +1,18 @@
 const MongoClient = require("mongodb").MongoClient;
 
-const URL_TEST = "mongodb://127.0.0.1/discover";
+const URL_TEST = "mongodb://127.0.0.1/";
 
 const DB_USER = "m1p10meantokysandratra";
 const DB_PASSWORD = "QdTJxNIB4E1RBmgT";
 const DB_NAME = "cluster0.gezyv3f.mongodb.net";
 const CONNECTION_STRING = `mongodb+srv://m1p10meantokysandratra:QdTJxNIB4E1RBmgT@cluster0.gezyv3f.mongodb.net/?retryWrites=true&w=majority`;
 
-const mongoClient = new MongoClient(CONNECTION_STRING);
+const mongoClient = new MongoClient(URL_TEST);
 
 const client = (async () => {
   await mongoClient.connect();
   console.log("Successfully connected");
-  return mongoClient.db("test");
+  return mongoClient.db("discover");
 })();
 
 module.exports = { client };
--- users/usersController.js
+++ users/usersController.js
@@ -1,31 +1,37 @@
 const usersService = require("./usersService");
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 
 const addUser = async function (req, res, next) {
   try {
     const { _id, ...user } = req.body;
     await usersService.addUser(user);
-    res.json({ data: user, message: "Ressource created" });
+    //res.json({ data: user, message: "Ressource created" });
+    res.json(user)
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
 };
 
 const getAllUsers = async function (req, res, next) {
   try {
     const users = await usersService.getAllUsers();
-    res.json({ data: users, message: "Ressources found" });
+    //res.json({ data: users, message: "Ressources found" });
+    res.json(users)
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
 };
 
 const getUserById = async function (req, res, next) {
   try {
     const user = await usersService.getUserById(req.params.id);
-    res.json({ data: user, message: "Ressource found" });
+    //res.json({ data: user, message: "Ressource found" });
+    res.json(user)
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
 };
 
@@ -33,32 +39,40 @@ const getUserByLoginAndPassword = async function (req, res, next) {
   try {
     const user = await usersService.getUserByLoginAndPassword(req.body);
     if (user) {
-      let jwtToken = jwt.sign({ email: user.email, userId: user._id }, "jwt-secret-key", { expiresIn: "1h" });
+      //let jwtToken = jwt.sign({ email: user.email, userId: user._id }, "jwt-secret-key", { expiresIn: "1h" });
 
-      res.json({ data: { token: jwtToken, _id: user._id, role: user.role }, message: "Ressource found" });
+      //res.json({ data: { token: jwtToken, _id: user._id, role: user.role }, message: "Ressource found" });
+      res.json(user);
     } else {
-      res.json({ data: user, message: "Invalid login/password" });
+      //res.json({ data: user, message: "Invalid login/password" });
+      res.json(null);
     }
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
+  
 };
 
 const updateUser = async function (req, res, next) {
   try {
     const user = await usersService.updateUser(req.params.id, req.body);
-    res.json({ data: user, message: "Ressource updated" });
+    //res.json({ data: user, message: "Ressource updated" });
+    res.json(user)
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
 };
 
 const deleteUser = async function (req, res, next) {
   try {
     const user = await usersService.deleteUser(req.params.id);
-    res.json({ data: user, message: "Ressource deleted" });
+    //res.json({ data: user, message: "Ressource deleted" });
+    res.json(user)
   } catch (err) {
-    res.json({ error: err.message });
+    //res.json({ error: err.message });
+    res.json(error.message);
   }
 };
 
