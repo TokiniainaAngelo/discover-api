b57781d1b33f0537cf29a77d6bbdb0cc5f1f2550
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2023-08-03 21:46:11 +0300

    add socket

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
 const cors = require("cors");
 const authorize = require("./middlewares/jwtAuthentication");
 
@@ -9,10 +11,13 @@ const siteRouter = require("./sites/siteRoutes");
 const settingRouter = require("./setting/settingRoutes");
 const commentRouter = require("./comments/commentRoutes");
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.use("/api-status", (req, res) => res.json({ status: "API is OK import" }));
 app.use("/users", usersRouter);
@@ -22,6 +27,17 @@ app.use("/comment", authorize, commentRouter);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, function () {
+io.on("connection", function (socket) {
+  console.log("New User: " + socket.request.connection.remoteAddress);
+  socket.on("message", function (msg) {
+    console.log("Message: " + msg);
+  });
+
+  socket.on("disconnect", function () {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(port, function () {
   console.log("Server is running on port " + port);
 });
