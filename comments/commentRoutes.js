6b500e124241b6b11ccbe67543baf061504d6af7
Sandratra <3s.sandratra@gmail.com>
2023-08-05 21:12:24 +0300

    Update POST/GET comment

--- comments/commentRoutes.js
+++ comments/commentRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const commentController = require("./commentController");
 const router = express.Router();
 
-router.route("/").post(commentController.addComment).get(commentController.getAllComments);
+//router.route("/").post(commentController.addComment).get(commentController.getAllComments);
 router.route("/:id").get(commentController.getCommentById).put(commentController.updateComment).delete(commentController.deleteComment);
+
+router.route("/site/:id").post(commentController.addComment).get(commentController.getAllComments)
 
 module.exports = router;
