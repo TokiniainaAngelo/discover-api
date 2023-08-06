f52ea159957cd8f1adb7b9cdb92f8cc87785071a
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2023-08-06 19:42:40 +0300

    set connection string for db

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,9 +5,9 @@ const URL_TEST = "mongodb://127.0.0.1/";
 const DB_USER = "m1p10meantokysandratra";
 const DB_PASSWORD = "QdTJxNIB4E1RBmgT";
 const DB_NAME = "cluster0.gezyv3f.mongodb.net";
-const CONNECTION_STRING = `mongodb+srv://m1p10meantokysandratra:QdTJxNIB4E1RBmgT@cluster0.gezyv3f.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_STRING = "mongodb+srv://m1p10meantokysandratra:nNgY2fajzCzsyBpt@cluster0.2tcyvwy.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoClient = new MongoClient(URL_TEST);
+const mongoClient = new MongoClient(CONNECTION_STRING);
 
 const client = (async () => {
   await mongoClient.connect();
